6bf765dda3269dc16ffa15824e1b65c4a6223565
Thomas <mahlqvist@msn.com>
2020-04-29 14:37:43 +0200

    changed input to minutes

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-echo "How long will your session be?"
+echo "How many minutes do you want to study?"
 
-read session_time_sec
+read session_time_min
 
-session_time_min=$(( $session_time_sec / 60 ))
+echo "Time for a $session_time_min minute deep work seesion, get into focus mode!"
+
+session_time_sec=$(( $session_time_min * 60 ))
 
 sleep $session_time_sec && notify-send "You did an amazing $session_time_min min session, time for a break!"
+
+exit
